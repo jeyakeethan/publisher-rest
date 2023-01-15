05eb2982f4a6a1430a0f0e14f7f41cb305c836e6
Keethan <jeyakeethan@gmail.com>
2023-01-15 15:47:35 +0530

    signup feature added

---
+++ src/main/java/com/jk/publisher/authentication/FailToUpdateDBException.java
@@ -0,0 +1,8 @@
+package com.jk.publisher.authentication;
+
+public class FailToUpdateDBException extends Exception {
+	public FailToUpdateDBException(String message) {
+		super(message);
+	}
+}
+
--- src/main/java/com/jk/publisher/authentication/UserController.java
+++ src/main/java/com/jk/publisher/authentication/UserController.java
@@ -7,12 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin
 @RestController
+@RequestMapping("/user")
 public class UserController {
 
 	@Autowired
@@ -21,16 +22,30 @@ public class UserController {
 	public UserController(UserRepository repository) {
 		this.repository = repository;
 	}
+
+	@PostMapping(value = "/signup", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public User signup(@RequestBody User user) throws FailToUpdateDBException {
+		
+		if ((user.getUsername() != null) && (user.getEmail() != null)
+			&& (user.getFirstName() != null) && (user.getLastName() != null)
+		    && (user.getPassword() != null) && (user.getCountry() != null)) {
+			repository.save(user);
+		}
+		throw new FailToUpdateDBException("Something got wrong."
+				+ "Unable to create the user record, please try again later.");
+	}
 	
 	@PostMapping(value = "/login", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	public User login(@RequestBody User userObj) {
+	public User login(@RequestBody User userObj) throws UserNotFoundException {
 		
 		Optional<User> user = repository.findByUsername(userObj.getUsername());
 		if (user.isPresent() && user.get().getEncryptedPassword().equals(userObj.getPassword())) {
 			user.get().setAuthenticationToken(new AuthenticationToken(Arrays.asList("Dashboard", "ReadContent", "ReadPremiumContent")));
 			return user.get();
 		}
-		return null;
+		
+		throw new UserNotFoundException("Please check with the credetials. No maching user found for the user "
+		+ userObj.getUsername() + "and the password you entered.");
 	}
 
 }
