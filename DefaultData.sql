b65030f0324fe0447766473f8391a51188c7e721
Jeyakeethan Jeyaganeshan <jeyakeethan@gmail.com>
2025-03-05 22:28:56 +0530

    Add files via upload

---
+++ DefaultData.sql
@@ -0,0 +1,104 @@
+INSERT INTO publisher.categories (category) VALUES
+	 ('Finance'),
+	 ('Weather Change'),
+	 ('Blog posts'),
+	 ('Emails'),
+	 ('Social media content'),
+	 ('Guides'),
+	 ('Ebooks'),
+	 ('Video content'),
+	 ('Webinars'),
+	 ('Case studies');
+INSERT INTO publisher.categories (category) VALUES
+	 ('Finance'),
+	 ('Weather Change'),
+	 ('Blog posts'),
+	 ('Emails'),
+	 ('Social media content'),
+	 ('Guides'),
+	 ('Ebooks'),
+	 ('Video content'),
+	 ('Webinars'),
+	 ('Case studies');
+INSERT INTO publisher.articles (author,content,footer_content,imageurl,read_time,title,category) VALUES
+	 ('Jeyakeethan','Rising global average temperature is associated with widespread changes in weather patterns. Scientific studies indicate that extreme weather events such as heat waves and large storms are likely to become more frequent or more intense with human-induced climate change. This chapter focuses on observed changes in temperature, precipitation, storms, floods, and droughts.
+
+Why does it matter?
+Long-term changes in climate can directly or indirectly affect many aspects of society in potentially disruptive ways. For example, warmer average temperatures could increase air conditioning costs and affect the spread of diseases like Lyme disease, but could also improve conditions for growing some crops. More extreme variations in weather are also a threat to society. More frequent and intense extreme heat events can increase illnesses and deaths, especially among vulnerable populations, and damage some crops. While increased precipitation can replenish water supplies and support agriculture, intense storms can damage property, cause loss of life and population displacement, and temporarily disrupt essential services such as transportation, telecommunications, energy, and water supplies.'',''Summary of Key Points
+U.S. and Global Temperature. Average temperatures have risen across the contiguous 48 states since 1901, with an increased rate of warming over the past 30 years. Nine of the top 10 warmest years on record have occurred since 1998. Average global temperatures show a similar trend, and all of the top 10 warmest years on record worldwide have occurred since 2005.'',''not available'',5','This is footer content sample','image url',0,'This is my first art',2),
+	 ('j','Content 2','This is footer content sample','',11,'Article 2',2),
+	 ('Jeyakeethan','one of the most visible facets of Object-relational mismatch. Object oriented systems can model both “is a” and “has a” relationship. Relational model supports only “has a” relationship between two entities. Hibernate can help you map such Objects with relational tables. But you need to choose certain mapping strategy based on your needs.
+
+One Table Per Class Hierarchy example
+Suppose we have a class Person with subclass Employee. The properties of each class are:
+
+* class Person
+	- firstname
+	- lastname
+
+* class Employee
+	- joining_date
+	- department_name
+In One Table per Class Hierarchy scheme, we store all the class hierarchy in a single SQL table. A discriminator is a key to uniquely identify the base type of the class hierarchy.
+
+Following are the advantages and disadvantages of One Table per Class Hierarchy scheme.
+
+Advantage
+This hierarchy offers the best performance even for in the deep hierarchy since single select may suffice.
+
+Disadvantage
+Changes to members of the hierarchy require column to be altered, added or removed from the table.
+
+','','image url 2',0,'This is my first article',3),
+	 ('Jeyakeethan','Weather describes the conditions outside right now in a specific place. For example, if you see that it’s raining outside right now, that’s a way to describe today’s weather. Rain, snow, wind, hurricanes, tornadoes — these are all weather events.
+
+Climate, on the other hand, is more than just one or two rainy days. Climate describes the weather conditions that are expected in a region at a particular time of year.
+
+Is it usually rainy or usually dry? Is it typically hot or typically cold? A region’s climate is determined by observing its weather over a period of many years—generally 30 years or more.
+
+So, for example, one or two weeks of rainy weather wouldn’t change the fact that Phoenix typically has a dry, desert climate. Even though it’s rainy right now, we still expect Phoenix to be dry because that''s what is usually the case.
+
+Want to know more about the difference between weather and climate? Take a look at this video!
+
+What Is Climate Change?
+Alaska''s Muir glacier in August 1941 and August 2004.
+Alaska''s Muir glacier in August 1941 and August 2004. Significant changes occurred in the 63 years between these two photos. Credit: USGS
+
+Climate change describes a change in the average conditions — such as temperature and rainfall — in a region over a long period of time. For example, 20,000 years ago, much of the United States was covered in glaciers. In the United States today, we have a warmer climate and fewer glaciers.
+
+Global climate change refers to the average long-term changes over the entire Earth. These include warming temperatures and changes in precipitation, as well as the effects of Earth’s warming, such as:
+
+Rising sea levels
+Shrinking mountain glaciers
+Ice melting at a faster rate than usual in Greenland, Antarctica and the Arctic
+Changes in flower and plant blooming times.
+Earth’s climate has constantly been changing — even long before humans came into the picture. However, scientists have observed unusual changes recently. For example, Earth’s average temperature has been increasing much more quickly than they would expect over the past 150 years.','This is footer content sample',NULL,0,'What is climate change?',2),
+	 ('Jeyakeethan','Climate change is already having visible effects on the world. The Earth is warming, rainfall patterns are changing, and sea levels are rising. These changes can increase the risk of heatwaves, floods, droughts, and fires.
+
+What are the effects of climate change?
+A changing climate impacts crop growth and human health, while many people may need to leave their homes. It places certain species at an increased risk of extinction. The effects of climate change are real, and they are already happening.
+
+The level of climate change we will see depends on how quickly we cut emissions of dangerous greenhouse gases. Even if we were to stop all emissions today, we would not prevent some changes. However, the sooner we cut emissions, the smaller the changes will be.
+
+
+
+Drivers of climate change
+We know that greenhouse gases, aerosol emissions and land use affect our climate. Overall, human activity is warming our planet.
+
+Changes to the climate system
+Climate change can affect our climate system in lots of different ways:
+
+Changes in the hydrological cycle
+Warmer land and air
+Warming oceans
+Melting sea ice and glaciers
+Rising sea levels
+Ocean acidification
+Global greening
+Changes in ocean currents
+More extreme weather
+Find out more about these and other indicators of climate change on our global climate dashboard and extremes dashboard.
+
+',NULL,NULL,1,'Effects of climate change',2);
+
+
